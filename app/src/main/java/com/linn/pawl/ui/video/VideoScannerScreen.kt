5fe600401a1284680a488d2c428c4901a8a3e7b0
iris_spp <iris.song@derbysoft.net>
2026-07-10 17:03:00 +0800

    update ui

--- app/src/main/java/com/linn/pawl/ui/video/VideoScannerScreen.kt
+++ app/src/main/java/com/linn/pawl/ui/video/VideoScannerScreen.kt
@@ -12,9 +12,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -82,12 +84,14 @@ fun VideoScannerScreen(
                         text = "VM-LIKE",
                         fontSize = 20.sp,
                         fontWeight = FontWeight.Bold,
+                        modifier = Modifier.offset(y = 8.dp)
                     )
                 },
                 colors = TopAppBarDefaults.topAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primary,
                     titleContentColor = MaterialTheme.colorScheme.onPrimary
                 ),
+                windowInsets = WindowInsets(top = 18.dp)
             )
         }
     ) { innerPadding ->
